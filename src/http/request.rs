964f4608da77005561a8a4608f59f57187d68b37
Jiri Novotny <hello@jiricodes.com>
2020-12-28 13:44:38 +0200

    traits

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,7 +1,13 @@
 use super::method::Method;
+use std::convert::TryFrom;
 	
 pub struct Request {
 	path: String,
 	query: Option<String>,
 	method: Method
+}
+
+impl TryFrom<&[u8]> for Request {
+	type Error = String;
+	
 }
