c602476aeca74925dbe4790efec8cf7067dcb1ba
Jiri Novotny <hello@jiricodes.com>
2020-12-29 07:13:04 +0200

    routing

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,11 +1,19 @@
 use super::server::Handler;
-use super::http::{Request, Response, StatusCode};
+use super::http::{Request, Response, StatusCode, Method};
 
 pub struct WebsiteHandler;
 
 impl Handler for WebsiteHandler {
 	fn handle_request(&mut self, request: &Request) -> Response {
 		dbg!(request);
-		Response::new(StatusCode::Ok, Some("<h1>jiricodes()</h1><p>This is msg from handler!</p>\n".to_string()))
+
+		match request.method() {
+			Method::GET => match request.path() {
+				"/" => Response::new(StatusCode::Ok, Some("<h1>jiricodes()</h1><p>This is msg matched to path in handler!</p>\n".to_string())),
+				"/hello" => Response::new(StatusCode::Ok, Some("<h1>jiricodes()</h1><p>Hello!</p>\n".to_string())),
+				_ => Response::new(StatusCode::NotFound, None)
+			}
+			_ => Response::new(StatusCode::NotFound, None)
+		}
 	}
 }
